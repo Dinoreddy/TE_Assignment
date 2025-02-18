12b604ea2c5fd3120aeee11e0472d5ac93270759
Dinoreddy <dinolt.ajay@gmail.com>
2025-02-18 19:11:59 +0530

    Updated frontend

--- src/lib/AxiosInstance.js
+++ src/lib/AxiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/projects" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/projects" : "https://te-assignment-backend.onrender.com/api/projects",
   headers: {
     "Content-Type": "application/json",
   },
