80328d9536491d0f028f8557df34383172ac3f07
Dinoreddy <dinolt.ajay@gmail.com>
2025-04-15 13:14:33 +0530

    added logout button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -88,14 +88,13 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen mt-40 p-4 sm:p-6 lg:p-8 relative">
-      {/* Logout Button */}
+      <button
+        onClick={handleLogout}
+        className="w-full sm:w-64 bg-red-600 hover:bg-red-500 text-white py-2 px-4 rounded-lg shadow-md text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mb-4"
+      >
+        Logout
+      </button>{" "}
       <div className="max-w-7xl mx-auto space-y-6">
-        <button
-          onClick={handleLogout}
-          className="w-full sm:w-64 bg-red-600 hover:bg-red-500 text-white py-2 px-4 rounded-lg shadow-md text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mb-4"
-        >
-          Logout
-        </button>
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
           <Link to="/create" className="self-start">
             <button className="w-full sm:w-auto bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 rounded-lg">
@@ -269,7 +268,6 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-
       {/* Delete Confirmation Modal */}
       {modalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4">
